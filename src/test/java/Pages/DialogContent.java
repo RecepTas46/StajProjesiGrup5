84b85ec7524ad5e7a6589c2de4a38b29715d6d8e
RecepTas46 <trecep2364@gmail.com>
2021-07-11 15:36:15 +0200

    projeGüncelleme

--- src/test/java/Pages/DialogContent.java
+++ src/test/java/Pages/DialogContent.java
@@ -10,28 +10,25 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 
-public class DialogContent extends Parent {
+public class DialogContent extends Parent{
+
     public DialogContent() {
         PageFactory.initElements(BaseDriver.getDriver(), this);
     }
 
-    @FindBy(id = "mat-input-0")
+    @FindBy(id="mat-input-0")
     private WebElement username;
 
-    @FindBy(id = "mat-input-1")
+    @FindBy(id="mat-input-1")
     private WebElement password;
 
-    @FindBy(css = "button[aria-label='LOGIN']")
+    @FindBy(css="button[aria-label='LOGIN']")
     private WebElement loginButton;
-
-    @FindBy(xpath = "//button[text()='Accept all cookies']")
-    private WebElement acceptCookies;
 
     @FindBy(xpath = "(//span[contains(text(),'Dashboard')])[2]")
     public WebElement dashboard;
 
     @FindBy(xpath = "//ms-add-button[contains(@tooltip,'TITLE.ADD')]//button")
-//stalElement hatasi ayni lokator den onceki sayfa da bir adet daha bulmasi..
     private WebElement addButton;
 
     @FindBy(xpath = "//ms-text-field[@formcontrolname='name']//input")
@@ -87,68 +84,87 @@ public class DialogContent extends Parent {
 
     @FindBy(xpath = "//ms-text-field[@formcontrolname='budgetAccountIntegrationCode']/input")
     private WebElement integrationCode;
+
+    @FindBy(xpath = "(//button[@class='consent-give'])[1]")
+    private WebElement acceptCookies;
+
     @FindBy(xpath = "//ms-edit-button//button")
     private WebElement editBtn;
 
+    @FindBy(css = "div>mat-option>span")
+    private List<WebElement> countryAllOptions;
+
+    @FindBy(xpath = "//mat-select[contains(@aria-labelledby,'mat-form-field')]")
+    private WebElement countrySelect;
+
+    @FindBy(xpath = "//ms-text-field[@formcontrolname='name']//input")
+    private WebElement cityName;
+
+    @FindBy(xpath = "//div[contains(text(),'deleted')]")
+    private WebElement cannotDeletedMsg;
+
+
+
     WebElement myElement;
+    public void findAndSend(String elementName,String value)
+    {
+          switch (elementName) {
+              case "username":
+                 myElement = username;
+                 break;
 
-    public void findAndSend(String elementName, String value) {//text ise yani veri gonderecek isek buraya ekliyoruz
-//        if (elementName=="username")
-//            myElement=username;
+              case "password":
+                  myElement = password;
+                  break;
 
-        switch (elementName) {
+              case "nameInput":
+                  myElement = nameInput;
+                  break;
 
-            case "username":
-                myElement = username;
-                break;
-            case "password":
-                myElement = password;
-                break;
-            case "nameInput":
-                myElement = nameInput;
-                break;
+              case "codeInput":
+                  myElement = codeInput;
+                  break;
 
-            case "codeInput":
-                myElement = codeInput;
-                break;
-            case "shortName":
-                myElement = shortName;
-                break;
+              case "shortName":
+                  myElement = shortName;
+                  break;
 
-            case "searchInput":
-                myElement = searchInput;
-                break;
+              case "searchInput":
+                  myElement = searchInput;
+                  break;
 
-            case "priorityInput":
-                myElement = priorityInput;
-                break;
+              case "priorityCode":
+                  myElement = priorityCode;
+                  break;
 
-            case "priorityCode":
-                myElement = priorityCode;
-                break;
+              case "integrationCode":
+                  myElement = integrationCode;
+                  break;
 
-            case "integrationCode":
-                myElement = integrationCode;
-                break;
-        }
+              case "cityName":
+                  myElement = cityName;
+                  break;
 
-        sendKeysFunction(myElement, value);
+          }
+
+          sendKeysFunction(myElement, value);
     }
 
-    public void findAndClick(String elementName) {
+    public void findAndClick(String elementName)
+    {
         switch (elementName) {
             case "loginButton":
                 myElement = loginButton;
                 break;
+
             case "addButton":
                 myElement = addButton;
                 break;
-            case "acceptCookies":
-                myElement = acceptCookies;
-                break;
+
             case "saveButton":
                 myElement = saveButton;
                 break;
+
             case "closeDialog":
                 myElement = closeDialog;
                 break;
@@ -164,11 +180,21 @@ public class DialogContent extends Parent {
             case "deleteDialogBtn":
                 myElement = deleteDialogBtn;
                 break;
+
             case "rightScroll":
                 myElement = rightScroll;
                 break;
+
+            case "acceptCookies":
+                myElement= acceptCookies;
+                break;
+
             case "editBtn":
-                myElement = editBtn;
+                myElement= editBtn;
+                break;
+
+            case "countrySelect":
+                myElement= countrySelect;
                 break;
 
 
@@ -177,7 +203,8 @@ public class DialogContent extends Parent {
         clickFunction(myElement);
     }
 
-    public void findAndContainsText(String elementName, String msg) {
+    public void findAndContainsText(String elementName, String msg)
+    {
         switch (elementName) {
             case "successMessage":
                 myElement = successMessage;
@@ -186,15 +213,24 @@ public class DialogContent extends Parent {
             case "errorMessage":
                 myElement = errorMessage;
                 break;
+
             case "alreadyExist":
                 myElement = alreadyExist;
                 break;
+
+            case "cannotDeletedMsg":
+                myElement = cannotDeletedMsg;
+                break;
+
+
         }
 
         verifyContainsText(myElement, msg);
     }
 
-    public void findAndDelete(String deleteString) {
+
+    public void findAndDelete(String deleteString)
+    {
         // tıklatma
         scrollToUpElement(rightScroll);
         findAndSend("searchInput", deleteString);
@@ -207,22 +243,27 @@ public class DialogContent extends Parent {
         findAndClick("deleteDialogBtn");
     }
 
-
-
     List<WebElement> myList;
-
-    public void ChooseListElement(String listName, String option)//choose-->secim
+    public void ChooseListElement(String listName, String option)
     {
         switch (listName) {
             case "userTypeAllOptions":
                 myList = userTypeAllOptions;
                 break;
+
+            case "countryAllOptions":
+                myList = countryAllOptions;
+                break;
+
+
         }
 
-        userType.click();
+       // userType.click();
         listSelectOption(myList, option);
     }
-    public void findAndEdit(String oldWord,String newWord) {
+
+    public void findAndEdit(String oldWord, String newWord)
+    {
         // tıklatma
         scrollToUpElement(rightScroll);
         findAndSend("searchInput", oldWord);
@@ -232,7 +273,17 @@ public class DialogContent extends Parent {
         waitnumberOfElementsToBeLessThan(By.xpath("//ms-delete-button//button"), 5);
 
         findAndClick("editBtn");
-        findAndSend("nameInput",newWord);
+
+        findAndSend("nameInput", newWord);
         findAndClick("saveButton");
+    }
+
+    public void invisibilityOfElement(String elementName)
+    {
+        switch (elementName) {
+            case "successMessage": myElement = successMessage; break;
+
+        }
+        waitUntilInvisibility(myElement);
     }
 }
